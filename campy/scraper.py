9585ecb3ae16a4d5a66bdea6f7e9f68d58515ec0
saikaveti <saileshkaveti@hotmail.com>
2020-03-21 17:22:03 -0400

    scraper done

--- campy/scraper.py
+++ campy/scraper.py
@@ -5,14 +5,6 @@ import pandas as pd
 import re
 import requests
 
-id = "H0MA04192"
-cycle = 2020
-
-url = "https://www.fec.gov/data/candidate/{}/?cycle={}&election_full=true".format(id, cycle)
-
-print(url)
-print("------------------------------------------------------------------------")
-
 def get_slab_div(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "lxml")
@@ -26,26 +18,84 @@ def get_slab_div(url):
 
     return slab_div
 
-def get_total_reciepts_table(slab_div):
+def get_total_reciepts_html_table(slab_div):
     entity_div = slab_div.findAll("div", {"class": "entity__figure entity__figure--narrow", "id": "total-raised"})[0]
     figure = entity_div.findAll("figure")[0]
     html_table = figure.findAll("table", {"class": "simple-table"})[0]
 
     return html_table
 
-def get_total_spent_table(slab_div):
+def get_total_spent_html_table(slab_div):
     entity_div = slab_div.findAll("div", {"class": "entity__figure entity__figure--narrow", "id": "total-spent"})[0]
     figure = entity_div.findAll("figure")[0]
     html_table = figure.findAll("table", {"class": "simple-table"})[0]
 
     return html_table
 
-def get_cash_summary_table(slab_div):
+def get_cash_summary_html_table(slab_div):
     entity_div = slab_div.findAll("div", {"class": "entity__figure entity__figure--narrow", "id": "cash-summary"})[0]
     figure = entity_div.findAll("figure")[0]
     html_table = figure.findAll("table", {"class": "simple-table"})[0]
 
     return html_table
 
-slab_div = get_slab_div(url)
-print(get_cash_summary_table(slab_div))
+def convert_row(row):
+    columns = row.findAll("td")
+    row = [None] * 2
+
+    for i in range(len(list(columns))):
+        attribute = str(columns[i])
+        attribute = re.sub(r'<td(.|\n)*?>', '', attribute)
+        attribute = re.sub(r'<\/td>', '', attribute)
+        attribute = attribute.strip()
+        attribute = re.sub(r'<a(.|\n)*?>', '', attribute)
+        attribute = re.sub(r'<\/a>', '', attribute)
+        attribute = attribute.strip()
+        attribute = re.sub(r'<span(.|\n)*?>', '', attribute)
+        attribute = re.sub(r'<\/span>', '', attribute)
+        attribute = attribute.strip()
+        row[i] = attribute
+
+    return row
+
+def total_reciepts_table(ids, cycle):
+    index = ['Total receipts', 'Total contributions', 'Total individual contributions', 'Itemized individual contributions', 'Unitemized individual contributions', 'Party committee contributions', 'Other committee contributions', 'Candidate contributions', 'Transfers from other authorized committees', 'Total loans received', 'Loans made by candidate', 'Other loans', 'Offsets to operating expenditures', 'Other receipts']
+    df = pd.DataFrame(index=index, columns=ids)
+    for id in ids:
+        url = "https://www.fec.gov/data/candidate/{}/?cycle={}&election_full=true".format(id, cycle)
+        slab_div = get_slab_div(url)
+        html_table = get_total_reciepts_html_table(slab_div)
+        rows_scrape = html_table.findAll("tr")
+        rows_for_id = [convert_row(row) for row in rows_scrape]
+        for row in rows_for_id:
+            df.at[row[0], id] = row[1]
+
+    return df
+
+def total_spent_table(ids, cycle):
+    index = ['Total disbursements', 'Operating expenditures', 'Transfers to other authorized committees', 'Total contribution refunds', 'Individual refunds', 'Political party refunds', 'Other committee refunds', 'Total loan repayments', 'Candidate loan repayments', 'Other loan repayments', 'Other disbursements']
+    df = pd.DataFrame(index=index, columns=ids)
+    for id in ids:
+        url = "https://www.fec.gov/data/candidate/{}/?cycle={}&election_full=true".format(id, cycle)
+        slab_div = get_slab_div(url)
+        html_table = get_total_spent_html_table(slab_div)
+        rows_scrape = html_table.findAll("tr")
+        rows_for_id = [convert_row(row) for row in rows_scrape]
+        for row in rows_for_id:
+            df.at[row[0], id] = row[1]
+
+    return df
+
+def cash_summary_table(ids, cycle):
+    index = ['Ending cash on hand', 'Debts/loans owed to committee', 'Debts/loans owed by committee']
+    df = pd.DataFrame(index=index, columns=ids)
+    for id in ids:
+        url = "https://www.fec.gov/data/candidate/{}/?cycle={}&election_full=true".format(id, cycle)
+        slab_div = get_slab_div(url)
+        html_table = get_cash_summary_html_table(slab_div)
+        rows_scrape = html_table.findAll("tr")
+        rows_for_id = [convert_row(row) for row in rows_scrape]
+        for row in rows_for_id:
+            df.at[row[0], id] = row[1]
+
+    return df
