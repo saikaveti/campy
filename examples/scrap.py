721e60e74ff5c716e84478e77f6009531f17e6a2
saikaveti <saileshkaveti@hotmail.com>
2020-03-26 00:53:22 -0400

    more work done

--- examples/scrap.py
+++ examples/scrap.py
@@ -13,23 +13,30 @@ import requests
 house_ideology_df = pd.read_csv("./csv-files/govtrack-stats-2018-house-ideology.csv")
 senate_ideology_df = pd.read_csv("./csv-files/govtrack-stats-2018-senate-ideology.csv")
 
-f = open('congress-115.json')
-ids = json.load(f)
-
-source_of_funds_df = source_of_funds_distribution(ids, 2018)
-source_of_funds_df.to_csv("source-of-funds.csv")
-print(source_of_funds_df)
-print("Complete Source of Funds")
-pac_contribution_distribution_df = pac_contribution_distribution(ids, 2018)
-pac_contribution_distribution_df.to_csv("pac-contribution-distributions.csv")
-print(pac_contribution_distribution_df)
-print("Complete PAC Contribution Distributions")
-
-source_of_funds_df = pd.read_csv("source-of-funds.csv")
-pac_contribution_distribution_df = pd.read_csv("pac-contribution-distributions.csv")
+source_of_funds_df = pd.read_csv("./csv-files/source-of-funds.csv")
+pac_contribution_distribution_df = pd.read_csv("./csv-files/pac-contribution-distributions.csv")
 
 source_of_funds_df = source_of_funds_df.dropna(axis=1, how='all')
 pac_contribution_distribution_df = pac_contribution_distribution_df.dropna(axis=1, how='all')
 
-print(source_of_funds_df)
-print(pac_contribution_distribution_df)
+source_of_funds_df[source_of_funds_df.columns[1:]]  \
+    = source_of_funds_df[source_of_funds_df.columns[1:]].replace('[\$,]', '', regex=True).astype(float)
+
+pac_contribution_distribution_df[pac_contribution_distribution_df.columns[1:]]  \
+    = pac_contribution_distribution_df[pac_contribution_distribution_df.columns[1:]].replace('[\$,]', '', regex=True).astype(float)
+
+source_of_funds_df[source_of_funds_df.columns[1:]] \
+    = source_of_funds_df[source_of_funds_df.columns[1:]].div(source_of_funds_df[source_of_funds_df.columns[1:]].sum(axis=0), axis=1)
+pac_contribution_distribution_df[pac_contribution_distribution_df.columns[1:]] \
+    = pac_contribution_distribution_df[pac_contribution_distribution_df.columns[1:]].div(pac_contribution_distribution_df[pac_contribution_distribution_df.columns[1:]].sum(axis=0), axis=1)
+
+source_of_funds_df = source_of_funds_df.T
+pac_contribution_distribution_df = pac_contribution_distribution_df.T
+
+source_of_funds_df = source_of_funds_df[1:]
+pac_contribution_distribution_df = pac_contribution_distribution_df[1:]
+
+combined = pd.merge(source_of_funds_df, pac_contribution_distribution_df, left_index=True, right_index=True)
+combined.columns = ['Large Individual Contributions', 'Small Individual Contributions (< $200)', 'PAC Contributions', 'Other', 'Candidate self-financing', 'Labor', 'Business', 'Ideological']
+
+combined.to_csv("./csv-files/combined-contribution.csv")
