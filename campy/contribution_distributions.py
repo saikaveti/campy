ea7c05378ee3162d1b55b020447fbaa8a90b23a4
saikaveti <saileshkaveti@hotmail.com>
2020-03-25 21:19:49 -0400

    need to heavily rework the api

--- campy/contribution_distributions.py
+++ campy/contribution_distributions.py
@@ -18,26 +18,27 @@ def get_source_html_table_from_url(url):
         graphic_div = l_primary_div.findAll("div", {"class": "HorizontalStackedBar"})[0]
         stacked_bar_div = graphic_div.findAll("div", {"class": "HorizontalStackedBar--legend u-richtext u-mt4 u-mb4", "style": ""})[0]
         html_table = stacked_bar_div.findAll("table")[0]
-
         return html_table
     except:
-        raise ValueError(url)
+        return None
 
 def get_pac_html_table_from_url(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "lxml")
-    body = soup.findAll("body")[0]
-    main_div = body.findAll("div", {"class": "Main", "id": "main", "role": "main"})[0]
-    main_wrap_div = main_div.findAll("div", {"class": "Main-wrap l-padding u-mt2"})[0]
-    l_wrap_div = main_wrap_div.findAll("div", {"class": "l-wrap"})[0]
-    l_primary_secondary_div = l_wrap_div.findAll("div", {"class": "l-secondary-primary"})[0]
-    l_primary_div = l_primary_secondary_div.findAll("div", {"class": "l-primary"})[0]
-    column_div = l_primary_div.findAll("div", {"class": "u-richtext u-mt4 u-mb4"})[0]
-    graphic_div = column_div.findAll("div", {"class": "HorizontalStackedBar"})[0]
-    stacked_bar_div = graphic_div.findAll("div", {"class": "HorizontalStackedBar--legend u-richtext u-mt4 u-mb4", "style": ""})[0]
-    html_table = stacked_bar_div.findAll("table")[0]
-
-    return html_table
+    try:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "lxml")
+        body = soup.findAll("body")[0]
+        main_div = body.findAll("div", {"class": "Main", "id": "main", "role": "main"})[0]
+        main_wrap_div = main_div.findAll("div", {"class": "Main-wrap l-padding u-mt2"})[0]
+        l_wrap_div = main_wrap_div.findAll("div", {"class": "l-wrap"})[0]
+        l_primary_secondary_div = l_wrap_div.findAll("div", {"class": "l-secondary-primary"})[0]
+        l_primary_div = l_primary_secondary_div.findAll("div", {"class": "l-primary"})[0]
+        column_div = l_primary_div.findAll("div", {"class": "u-richtext u-mt4 u-mb4"})[0]
+        graphic_div = column_div.findAll("div", {"class": "HorizontalStackedBar"})[0]
+        stacked_bar_div = graphic_div.findAll("div", {"class": "HorizontalStackedBar--legend u-richtext u-mt4 u-mb4", "style": ""})[0]
+        html_table = stacked_bar_div.findAll("table")[0]
+        return html_table
+    except:
+        return None
 
 def convert_row(row):
     columns = row.findAll("td")
@@ -60,6 +61,8 @@ def source_of_funds_distribution(ids, cycle):
     for id in ids.keys():
         url = "https://www.opensecrets.org/members-of-congress/summary?cid={}&cycle={}".format(id, cycle)
         html_table = get_source_html_table_from_url(url)
+        if html_table is None:
+            continue
         tbody = html_table.findAll("tbody")[0]
         rows_scrape = html_table.findAll("tr")
         rows_for_id = [convert_row(row) for row in rows_scrape]
@@ -68,15 +71,14 @@ def source_of_funds_distribution(ids, cycle):
 
     return df
 
-def source_of_funds_distribution2(ids, cycle):
-
-
 def pac_contribution_distribution(ids, cycle):
     index = ['Labor', 'Business', 'Ideological']
     df = pd.DataFrame(index = index, columns = ids.values())
     for id in ids.keys():
         url = "https://www.opensecrets.org/members-of-congress/pacs?cid={}&cycle={}".format(id, cycle)
         html_table = get_pac_html_table_from_url(url)
+        if html_table is None:
+            continue
         tbody = html_table.findAll("tbody")[0]
         rows_scrape = html_table.findAll("tr")
         rows_for_id = [convert_row(row) for row in rows_scrape]
--- examples/scrap.py
+++ examples/scrap.py
@@ -1,8 +1,6 @@
 import sys
 sys.path.append("..")
 
-from itertools import islice
-
 from campy.id_query import get_ids_for_states
 from campy.contribution_distributions import source_of_funds_distribution, pac_contribution_distribution
 
@@ -15,12 +13,22 @@ import requests
 house_ideology_df = pd.read_csv("./csv-files/govtrack-stats-2018-house-ideology.csv")
 senate_ideology_df = pd.read_csv("./csv-files/govtrack-stats-2018-senate-ideology.csv")
 
-f = open('result.json')
+f = open('congress-115.json')
 ids = json.load(f)
 
-n_items = take(3, ids.iteritems())
-print(n_items)
-print(source_of_funds_distribution(ids, 2018))
+# source_of_funds_df = source_of_funds_distribution(ids, 2018)
+# source_of_funds_df.to_csv("source-of-funds.csv")
+# print("Complete Source of Funds")
+pac_contribution_distribution_df = pac_contribution_distribution(ids, 2018)
+pac_contribution_distribution_df.to_csv("pac-contribution-distributions.csv")
+print(pac_contribution_distribution_df)
+print("Complete PAC Contribution Distributions")
 
-
-#source_of_funds_df = source_of_funds_distribution(ids, 2018)
+# source_of_funds_df = pd.read_csv("source-of-funds.csv")
+# pac_contribution_distribution_df = pd.read_csv("pac-contribution-distributions.csv")
+#
+# source_of_funds_df = source_of_funds_df.dropna(axis=1, how='all')
+# pac_contribution_distribution_df = pac_contribution_distribution_df.dropna(axis=1, how='all')
+#
+# print(source_of_funds_df)
+# print(pac_contribution_distribution_df)
